ac6bd0ba1c7054289bb59a461f8b550eae3e69eb
luk1684tw <luk1684tw@gmail.com>
2019-05-16 19:25:05 +0800

    add debug info

--- datasets.py
+++ datasets.py
@@ -13,8 +13,10 @@ def GenerateCifar10Dataset(root, batch_size):
     training = torchvision.datasets.CIFAR10(os.path.join(root, 'datasets/cifar10'), download=True, train=True, transform=transform)
     testing = torchvision.datasets.CIFAR10(os.path.join(root, 'datasets/cifar10'), download=True, train=False, transform=transform)
 
+    print ('create sampler')
     sampler = WeightedRandomSampler([0.9, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], 10)
 
+    print ('create dataloader')
     trainLoader = torch.utils.data.DataLoader(training, batch_size, shuffle=False, num_workers=2, sampler=sampler)
     testLoader = torch.utils.data.DataLoader(testing, batch_size, shuffle=False, num_workers=2, sampler=sampler)
 
