010394040f5adf73c5f8ab3f99d8a817cdbf2488
luk1684tw <luk1684tw@gmail.com>
2019-05-16 19:17:00 +0800

    debug weightedrandomsampler import error

--- datasets.py
+++ datasets.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 import os
 from torchvision import transforms
-import torch.utils.data.WeightedRandomSampler as WeightedRandomSampler
+from  torch.utils.data import WeightedRandomSampler
 
 def GenerateCifar10Dataset(root, batch_size):
     transform = transforms.Compose([
