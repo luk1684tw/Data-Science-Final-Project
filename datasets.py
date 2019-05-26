56347f685e9954432a45f3dd83af26265fa3f827
luk1684tw <luk1684tw@gmail.com>
2019-05-26 16:48:47 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -19,7 +19,11 @@ def GenerateCifar10Dataset(root, trainBatchSize, testBatchSize, dist_index):
         [20, 20, 20, 20, 1, 1, 1, 1, 1, 1],
         [10, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [15, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [20, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        [20, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [30, 30, 30, 30, 1, 1, 1, 1, 1, 1],
+        [30, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [50, 50, 50, 50, 1, 1, 1, 1, 1, 1],
+        [50, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
     print('start create datasets')
     trainTransform = transforms.Compose([
