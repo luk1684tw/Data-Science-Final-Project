10ae4944d4e2dfc183b6a1b337297ee64104fd7c
ms0701515 <ms0701515@gmail.com>
2019-05-26 17:30:24 +0800

    [DEBUG]

--- l1-norm-pruning/main.py
+++ l1-norm-pruning/main.py
@@ -145,20 +145,21 @@ def test():
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-        pre_mask = torch.zeros(output.size()).scatter_(1, pred.cpu().view(-1, 1), 1.)
-        predict_num += pre_mask.sum(0)
-        tar_mask = torch.zeros(output.size()).scatter_(1, target.data.cpu().view(-1, 1), 1.)
-        target_num += tar_mask.sum(0)
-        acc_mask = pre_mask*tar_mask
-        acc_num += acc_mask.sum(0)
+        print(target.data.view_as(pred))
+        # pre_mask = torch.zeros(output.size()).scatter_(1, pred.cpu().view(-1, 1), 1.)
+        # predict_num += pre_mask.sum(0)
+        # tar_mask = torch.zeros(output.size()).scatter_(1, target.data.cpu().view(-1, 1), 1.)
+        # target_num += tar_mask.sum(0)
+        # acc_mask = pre_mask*tar_mask
+        # acc_num += acc_mask.sum(0)
 
-    recall = acc_num/target_num
-    precision = acc_num/predict_num
-    F1 = 2*recall*precision/(recall+precision)
+    # recall = acc_num/target_num
+    # precision = acc_num/predict_num
+    # F1 = 2*recall*precision/(recall+precision)
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%), F1 Score: {:.2f}\n'.format(
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)), F1)
+        100. * correct / len(test_loader.dataset)))
     return correct / float(len(test_loader.dataset))
 
 def save_checkpoint(state, is_best, filepath, dist):
