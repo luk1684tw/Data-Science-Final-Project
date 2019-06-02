2055d256149da07a851016b8e8f677ad1da80dc1
luk1684tw <luk1684tw@gmail.com>
2019-06-02 18:13:22 +0800

    decrease scratch E training epoch to 120

--- l1-norm-pruning/main_B.py
+++ l1-norm-pruning/main_B.py
@@ -25,7 +25,7 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=256, metavar='N',
                     help='input batch size for testing (default: 256)')
-parser.add_argument('--epochs', type=int, default=160, metavar='N',
+parser.add_argument('--epochs', type=int, default=120, metavar='N',
                     help='number of epochs to train (default: 160)')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
