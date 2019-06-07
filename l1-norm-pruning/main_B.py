db46fd817c14b168e9bbca1310572a9bb10c9136
ms0701515 <ms0701515@gmail.com>
2019-06-08 00:26:52 +0800

    plot

--- l1-norm-pruning/main_B.py
+++ l1-norm-pruning/main_B.py
@@ -156,39 +156,21 @@ def save_checkpoint(state, is_best, filepath):
     if is_best:
         shutil.copyfile(os.path.join(filepath, f'scratchB{args.dist}.pth.tar'), os.path.join(filepath, f'model{args.dist}_best.pth.tar'))
 
-def plot_kernels(tensor, num_cols=6):
-    if not tensor.ndim==4:
-        raise Exception("assumes a 4D tensor")
-    if not tensor.shape[-1]==3:
-        raise Exception("last dim needs to be 3 to plot")
-    num_kernels = tensor.shape[0]
-    num_rows = 1+ num_kernels // num_cols
-    fig = plt.figure(figsize=(num_cols,num_rows))
-    for i in range(tensor.shape[0]):
-        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        ax1.imshow(tensor[i])
-        ax1.axis('off')
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([])
-
-    plt.subplots_adjust(wspace=0.1, hspace=0.1)
-    plt.show()
+def plot_weights(tensor):
+    print(tensor)
 
 best_prec1 = 0.
 F1 = 0.
-for epoch in range(args.start_epoch, args.epochs):
+for epoch in range(0, 1):#range(args.start_epoch, args.epochs):
     if epoch in [int(args.epochs*0.5), int(args.epochs*0.75)]:
         for param_group in optimizer.param_groups:
             param_group['lr'] *= 0.1
     train(epoch)
 
     # Plot 
-    mm = model.double()
-    filters = mm.modules
-    body_model = [i for i in mm.children()][0]
-    layer1 = body_model[0]
-    tensor = layer1.weight.cpu().data.numpy()
-    plot_kernels(tensor)
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            plot_weights(m.weights.data)
 
     prec1, f1 = test()
     is_best = prec1 > best_prec1
