34d8cb6e1445c5401d4ab3684bb09563d4bccb44
ms0701515 <ms0701515@gmail.com>
2019-06-08 01:33:03 +0800

    plot

--- l1-norm-pruning/main_B.py
+++ l1-norm-pruning/main_B.py
@@ -161,18 +161,19 @@ def plot_kernels(tensor, num_cols=6):
     #     raise Exception("assumes a 4D tensor")
     # if not tensor.shape[-1]==3:
     #     raise Exception("last dim needs to be 3 to plot")
-    num_kernels = tensor.shape[0]
-    num_rows = 1+ num_kernels // num_cols
-    fig = plt.figure(figsize=(num_cols,num_rows))
-    for i in range(tensor.shape[0]):
-        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        ax1.imshow(tensor[i])
-        ax1.axis('off')
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([])
+    print(tensor.shape)
+    # num_kernels = tensor.shape[0]
+    # num_rows = 1+ num_kernels // num_cols
+    # fig = plt.figure(figsize=(num_cols,num_rows))
+    # for i in range(tensor.shape[0]):
+    #     ax1 = fig.add_subplot(num_rows,num_cols,i+1)
+    #     ax1.imshow(tensor[i])
+    #     ax1.axis('off')
+    #     ax1.set_xticklabels([])
+    #     ax1.set_yticklabels([])
 
-    plt.subplots_adjust(wspace=0.1, hspace=0.1)
-    plt.show()
+    # plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    # plt.show()
 
 best_prec1 = 0.
 F1 = 0.
