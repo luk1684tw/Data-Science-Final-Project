f3fda0776e5f965671f2fbb6268aaaf216d1096b
ms0701515 <ms0701515@gmail.com>
2019-06-08 01:03:34 +0800

    plot

--- l1-norm-pruning/main_B.py
+++ l1-norm-pruning/main_B.py
@@ -157,20 +157,22 @@ def save_checkpoint(state, is_best, filepath):
         shutil.copyfile(os.path.join(filepath, f'scratchB{args.dist}.pth.tar'), os.path.join(filepath, f'model{args.dist}_best.pth.tar'))
 
 def plot_kernels(tensor, num_cols=6):
-    print(tensor.shape)
-    # num_rows = 11
-    # fig = plt.figure(figsize=(num_cols,num_rows))
-    # i = 0
-    # for t in tensor:
-    #     ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-    #     pilTrans = transforms.ToPILImage()
-    #     pilImg = pilTrans(t)
-    #     ax1.imshow(pilImg, interpolation='none')
-    #     #print(tensor[i])
-    #     ax1.axis('off')
-    #     ax1.set_xticklabels([])
-    #     ax1.set_yticklabels([])
-    #     i+=1
+    if not tensor.ndim==4:
+        raise Exception("assumes a 4D tensor")
+    if not tensor.shape[-1]==3:
+        raise Exception("last dim needs to be 3 to plot")
+    num_kernels = tensor.shape[0]
+    num_rows = 1+ num_kernels // num_cols
+    fig = plt.figure(figsize=(num_cols,num_rows))
+    for i in range(tensor.shape[0]):
+        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
+        ax1.imshow(tensor[i])
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.show()
 
 best_prec1 = 0.
 F1 = 0.
