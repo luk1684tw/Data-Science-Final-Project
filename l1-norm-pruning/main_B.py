c2295fdf87bef60373f8b689bc860e5dab8863f8
ms0701515 <ms0701515@gmail.com>
2019-06-07 23:53:14 +0800

    plot

--- l1-norm-pruning/main_B.py
+++ l1-norm-pruning/main_B.py
@@ -18,6 +18,8 @@ from datasets import GenerateCifar10Dataset as get
 from sklearn.metrics import f1_score
 import models
 from compute_flops import print_model_param_flops
+from matplotlib import pyplot as plt
+
 modelRoot = '/content/Drive/My Drive/Colab Notebooks/models/pruned'
 datasetRoot = '/content/Drive/My Drive/Colab Notebooks'
 
@@ -154,6 +156,23 @@ def save_checkpoint(state, is_best, filepath):
     if is_best:
         shutil.copyfile(os.path.join(filepath, f'scratchB{args.dist}.pth.tar'), os.path.join(filepath, f'model{args.dist}_best.pth.tar'))
 
+def plot_kernels(tensor, num_cols=6):
+    if not tensor.ndim==4:
+        raise Exception("assumes a 4D tensor")
+    if not tensor.shape[-1]==3:
+        raise Exception("last dim needs to be 3 to plot")
+    num_kernels = tensor.shape[0]
+    num_rows = 1+ num_kernels // num_cols
+    fig = plt.figure(figsize=(num_cols,num_rows))
+    for i in range(tensor.shape[0]):
+        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
+        ax1.imshow(tensor[i])
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.show()
 
 best_prec1 = 0.
 F1 = 0.
@@ -162,6 +181,16 @@ for epoch in range(args.start_epoch, args.epochs):
         for param_group in optimizer.param_groups:
             param_group['lr'] *= 0.1
     train(epoch)
+
+    # Plot 
+    mm = model.double()
+    filters = mm.modules
+    body_model = [i for i in mm.children()][0]
+    layer1 = body_model[0]
+    tensor = layer1.weight.data.numpy()
+    plot_kernels(tensor)
+
+    plot_weights()
     prec1, f1 = test()
     is_best = prec1 > best_prec1
     best_prec1 = max(prec1, best_prec1)
