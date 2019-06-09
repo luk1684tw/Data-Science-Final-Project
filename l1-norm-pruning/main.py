ca1aa43cbc124e7815934bd501a13102ffb84730
ms0701515 <ms0701515@gmail.com>
2019-06-09 19:59:19 +0800

    Try

--- l1-norm-pruning/main.py
+++ l1-norm-pruning/main.py
@@ -23,13 +23,13 @@ import matplotlib
 gui_env = ['TKAgg','GTKAgg','Qt4Agg','WXAgg']
 for gui in gui_env:
     try:
-        print "testing", gui
+        print ("testing", gui)
         matplotlib.use(gui,warn=False, force=True)
         from matplotlib import pyplot as plt
         break
     except:
         continue
-print "Using:",matplotlib.get_backend()
+print ("Using:",matplotlib.get_backend())
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR training')
 parser.add_argument('--dataset', type=str, default='cifar100',
