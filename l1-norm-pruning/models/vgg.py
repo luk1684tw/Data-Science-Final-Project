5d7f918c2fef60cfada84615548f0c2e1329b9d0
luk1684tw <luk1684tw@gmail.com>
2019-06-11 19:40:08 +0800

    print x.shape

--- l1-norm-pruning/models/vgg.py
+++ l1-norm-pruning/models/vgg.py
@@ -56,6 +56,7 @@ class vgg(nn.Module):
         x = self.feature(x)
         x = nn.AvgPool2d(2)(x)
         x = x.view(x.size(0), -1)
+        print ('[INFO] Shape of X is', x.shape())
         y = self.classifier(x)
         return y
 
