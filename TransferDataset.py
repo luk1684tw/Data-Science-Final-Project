8ab318a0d682f2c7c2ea8378c8f2d83ee39606bf
luk1684tw <luk1684tw@gmail.com>
2019-06-11 17:38:56 +0800

    fix OCT dataset transforms

--- TransferDataset.py
+++ TransferDataset.py
@@ -3,4 +3,43 @@ import torchvision
 import os
 import random
 from torchvision import transforms
-from torch.utils.data import WeightedRandomSampler
+
+'''
+define x > y
+distribution A:[x, y, y, y, y, y, y, y, y, y] 
+distribution B:[x, x, x, x, y, y, y, y, y, y] 
+distribution C:[x, x, x, x, x, x, x, x, x, y] 
+
+'''
+
+
+def GenerateOCTDatasets(root, trainBatchSize, testBatchSize):
+    print('[INFO] Start creating datasets')
+    trainTransform = transforms.Compose([
+        transforms.Resize(size=(224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010))
+    ])
+
+    testTransform = transforms.Compose([
+        transforms.Resize(size=(224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010))
+    ])
+
+    training = torchvision.datasets.ImageFolder(os.path.join(
+        root, 'datasets/OCT2017/train'), transform=trainTransform)
+    testing = torchvision.datasets.ImageFolder(os.path.join(
+        root, 'datasets/OCT2017/test'), transform=testTransform)
+
+    print('[INFO] Creating dataloader')
+    trainLoader = torch.utils.data.DataLoader(
+        training, trainBatchSize, shuffle=True, num_workers=2)
+    testLoader = torch.utils.data.DataLoader(
+        testing, testBatchSize, shuffle=True, num_workers=2)
+
+    print('[INFO] Return dataloaders')
+    return trainLoader, testLoader
