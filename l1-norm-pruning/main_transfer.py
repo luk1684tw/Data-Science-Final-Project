a7263ff3f39890c6e5a34f129555ae30061927a3
ms0701515 <ms0701515@gmail.com>
2019-06-13 04:36:13 +0800

    Need to add dist in args to store checkpoint

--- l1-norm-pruning/main_transfer.py
+++ l1-norm-pruning/main_transfer.py
@@ -58,7 +58,8 @@ parser.add_argument('--depth', default=16, type=int,
                     help='depth of the neural network')
 parser.add_argument('--method', default=0, type=int,
                     help='distribution of dataset')            
-        
+parser.add_argument('--dist', default=0, type=str, 
+                    help='distribution of dataset')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
