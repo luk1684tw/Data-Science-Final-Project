428760dc396a0fbd57333fdf0c4a8e32aa163e5c
luk1684tw <luk1684tw@gmail.com>
2019-06-18 13:05:24 +0800

    load state_dict

--- l1-norm-pruning/main_transfer.py
+++ l1-norm-pruning/main_transfer.py
@@ -84,7 +84,7 @@ if args.scratch:
     print ('[INFO] Loading model from', modelPath)
     checkpoint = torch.load(modelPath)
     model = models.__dict__[args.arch](dataset=args.dataset, depth=args.depth, cfg=checkpoint['cfg'], transfer=True, method=args.method)
-
+    model.load_state_dict(checkpoint['state_dict'])
 
 print('Old model features: ', model.classifier[-1].out_features) 
 # Freeze training for all layers
